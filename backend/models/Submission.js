c8a1c0a904fe873874e95cf4d2588fc9d3b87c6d
Raphael9143 <s3jack3s@gmail.com>
2025-09-20 08:40:34 +0700

    [feat] model submission

---
+++ backend/models/Submission.js
@@ -0,0 +1,71 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Submission = sequelize.define('Submission', {
+  id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  class_assignment_id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    allowNull: false
+  },
+  student_id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    allowNull: false
+  },
+  submission_time: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  attempt_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1
+  },
+  file_path: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+  ocl_constraints: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  check_result: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  score: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  feedback: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.ENUM('submitted', 'graded', 'late', 'resubmitted'),
+    allowNull: false,
+    defaultValue: 'submitted'
+  },
+  is_final: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  tableName: 'submissions',
+  timestamps: false
+});
+
+module.exports = Submission;
