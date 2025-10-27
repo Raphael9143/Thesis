30a0fd4fdd16fe4bdcc92c90ecbd43684f8cddbc
Raphael9143 <s3jack3s@gmail.com>
2025-10-27 14:54:08 +0700

    [feat] exam model

---
+++ backend/models/Exam.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Exam = sequelize.define('Exam', {
+  id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  course_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  start_time: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  end_time: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  model_file: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    field: 'updated_at'
+  }
+}, {
+  tableName: 'exams',
+  timestamps: true,
+  underscored: true
+});
+
+const Course = require('./Course');
+Exam.belongsTo(Course, { foreignKey: 'course_id', as: 'course' });
+
+module.exports = Exam;
