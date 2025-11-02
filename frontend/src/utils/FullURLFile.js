14d73e72df60f7dc3c900839adc9aa85300a9eca
Raphael9143 <s3jack3s@gmail.com>
2025-11-02 16:00:10 +0700

    [refactor] to full url utility

---
+++ frontend/src/utils/FullURLFile.js
@@ -0,0 +1,16 @@
+import axiosClient from '../../services/axiosClient';
+
+const toFullUrl = (path) => {
+	if (!path) return path;
+	if (/^https?:\/\//i.test(path)) return path;
+	try {
+		const base = axiosClient.defaults.baseURL || '';
+		const origin = new URL(base).origin;
+		if (path.startsWith('/')) return origin + path;
+		return origin + '/' + path.replace(/^\/+/, '');
+	} catch (err) {
+		return path;
+	}
+}
+
+export default toFullUrl;
