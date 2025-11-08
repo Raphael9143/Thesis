687f4a134f26543312a0838cc22c2f16e67e9661
Raphael9143 <s3jack3s@gmail.com>
2025-11-08 09:39:24 +0700

    [update] eslint config max len

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig([
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    rules: {
+      "max-len": ["error", { code: 80 }],
+    },
   },
   {
     files: [
