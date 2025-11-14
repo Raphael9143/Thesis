1b7c02c017be3c1580dd5585ce05ffa2a225b78f
Raphael9143 <s3jack3s@gmail.com>
2025-11-14 16:27:05 +0700

    [feat] project member component

---
+++ frontend/src/components/researcher/ProjectMembers.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import UserRow from './UserRow';
+
+export default function ProjectMembers({ members }) {
+  return (
+    <div style={{ textAlign: 'left' }}>
+      <div className="font-700" style={{ marginBottom: 8 }}>
+        Contributors
+      </div>
+      {members ? (
+        <div>
+          <div style={{ marginBottom: 12 }}>
+            {members.owner ? <UserRow user={members.owner} role="Owner" /> : <div>-</div>}
+          </div>
+
+          <div style={{ margin: '12px 0' }}>
+            {Array.isArray(members.moderators) && members.moderators.length > 0 ? (
+              members.moderators.map((m) => <UserRow key={m.id} user={m} role="Moderator" />)
+            ) : (
+              <div>None</div>
+            )}
+          </div>
+
+          <div style={{ marginTop: 12 }}>
+            {Array.isArray(members.contributors) && members.contributors.length > 0 ? (
+              members.contributors.map((m) => <UserRow key={m.id} user={m} role="Contributor" />)
+            ) : (
+              <div>None</div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div>Loading members...</div>
+      )}
+    </div>
+  );
+}
