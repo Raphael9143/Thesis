4faba0f42c45bfe616e59f27b4745a7c8c39319e
Raphael9143 <s3jack3s@gmail.com>
2025-11-14 15:06:52 +0700

    [feat] research project model

---
+++ backend/models/ResearchProject.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const ResearchProject = sequelize.define(
+  "ResearchProject",
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM("DRAFT", "ACTIVE", "CLOSED"),
+      allowNull: false,
+      defaultValue: "DRAFT",
+    },
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: "owner_id",
+      comment: "User id of project owner",
+    },
+    mainUseModelId: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: true,
+      field: "main_use_model_id",
+      comment: "Reference to primary UseModel for this project",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: "created_at",
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: "updated_at",
+    },
+  },
+  {
+    tableName: "research_projects",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = ResearchProject;
